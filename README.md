3be1c2e6fa76d4b7a356b85b101f3167f0543532
funnyhun <funnyhun0222@gmail.com>
2025-09-26 15:51:07 +0900

    deploy : gh-pages for testing UI

--- README.md
+++
@@ -1,5 +0,0 @@
-# React + vite(svgr) + styled-components + few types
-
-Simple Frontend Dev template
-
-by : funnyhun
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import vitePluginSvgr from "vite-plugin-svgr";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), vitePluginSvgr()],
-});
