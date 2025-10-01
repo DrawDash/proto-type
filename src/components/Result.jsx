21b710e32b33ed883f370dd4736864057d954f78
funnyhun <funnyhun0222@gmail.com>
2025-10-01 18:33:27 +0900

    feat : Result component to provide subject

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,14 +2,20 @@ import styled from "styled-components";
 import { Box } from "./ui/Box";
 
 const Wrapper = styled.div`
+  width: 100%;
   display: flex;
   flex-wrap: wrap;
+  gap: 1rem;
 `;
 
-export const Result = () => {
+export const Result = ({ result }) => {
   return (
     <Wrapper>
-      <Box title={"title"} category={"category"} contents={"contents"} />
+      {result !== null
+        ? result.map((e, i) => {
+            return <Box key={i} title={e.title} category={e.category} contents={e.contents} />;
+          })
+        : null}
     </Wrapper>
   );
 };
--- src/components/ui/Box.jsx
+++ src/components/ui/Box.jsx
@@ -1,11 +1,36 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  width: 100%;
+
+  @media (min-width: 1024px) {
+    width: 49%;
+  }
+
+  min-height: 8rem;
+  padding: 1rem;
+
   display: flex;
+  flex-direction: column;
+
+  border: 0.1rem solid ${({ theme }) => theme.borderColor};
+  border-radius: 0.6rem;
 `;
 
 const BoxInfo = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & > span {
+    padding: 0.1rem 0.2rem;
+    font-size: 0.8rem;
+    color: ${({ theme }) => theme.subColor};
+
+    border: 0.1rem solid ${({ theme }) => theme.borderColor};
+    background-color: ${({ theme }) => theme.borderColor};
+    border-radius: 0.8rem;
+  }
 `;
 
 export const Box = ({ title, category, contents }) => {
