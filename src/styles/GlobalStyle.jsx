c0493d3a3436a424eeedb2f928fd43eabeb53313
funnyhun <funnyhun0222@gmail.com>
2025-10-13 22:15:42 +0900

    style : add more detail definitions

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,12 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-  html {
+  :root {
     font-size : 10px;
+	font-family: 'Noto Sans', sans-serif;
+	color: ${({ theme }) => theme.color};
   }
 
-  @media (min-width: 768px) { html { font-size: 12px; } }
-  @media (min-width: 1024px) { html { font-size: 16px; } }
+  @media (min-width: 600px) { :root { font-size: 12px; } }
+  @media (min-width: 768px) { :root { font-size: 14px; } }
+  @media (min-width: 1024px) { :root { font-size: 16px; } }
 
   html, body, #root {
     height: 100%;
@@ -20,14 +23,21 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background-color: ${({ theme }) => theme.themeColor}
+    background-color: ${({ theme }) => theme.themeColor};
+    font-size: 1rem;
+	font-weight: 400;
   }
 
   h1, h2, h3, h4, h5, h6 {
-    margin: 0;
     font-weight: 400;
-    color: ${({ theme }) => theme.color}
   }
+
+  h1 { font-size: 2.5rem; }
+  h2 { font-size: 2rem; }
+  h3 { font-size: 1.5rem; }
+  h4 { font-size: 1.1rem; }
+  h5 { font-size: 1rem; }
+  h6 { font-size: 1rem; }
 
   span {
     line-height: 1;
@@ -38,6 +48,24 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   button {
-	cursor: pointer;
+    cursor: pointer;
+  }
+
+  textarea, button, select {
+    font-family: inherit;
+    font-size: 1rem; 
+    color: ${({ theme }) => theme.color};
+  }
+
+  input, option, ul, ol{
+    font-family: inherit;
+    font-size: 0.9rem;
+    color: ${({ theme }) => theme.color};
+  }
+
+  ul, ol {
+	list-style-position: inside;
+	font-size: 0.8rem;
+	color: ${({ theme }) => theme.subColor};
   }
 `;
