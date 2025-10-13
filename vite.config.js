ad409cc6b26ff7d1f72dd292b2339435c0aedd1a
funnyhun <funnyhun0222@gmail.com>
2025-10-14 01:01:12 +0900

    build : add basicSsl for https suport in dev envirment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import vitePluginSvgr from "vite-plugin-svgr";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), vitePluginSvgr()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  const API_TARGET = env.VITE_API_ENDPOINT;
+
+  return {
+    plugins: [react(), vitePluginSvgr(), basicSsl()],
+    server: {
+      https: true,
+      host: true,
+      proxy: {
+        "/api": {
+          target: API_TARGET,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
